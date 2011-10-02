c0f5d7e58229e1990c19c457bde34aafc6d7ff11
Lin Jen-Shin <godfat@godfat.org>
2011-10-02 18:14:31 +0800

    tidyup.rb: fullwidth support!

--- lib/tidyup.rb
+++ lib/tidyup.rb
@@ -11,14 +11,35 @@ module Tidyup
 
   def self.break_lines words
     words.inject(['']){ |r, i|
-      if r.last.size + i.size < Tidyup.terminal_width
-        r.last << "#{i} "
+      if word_size(r.last) + word_size(i) < terminal_width
+        if double_width?(i)
+          r.last << i
+        else
+          r.last << "#{i} "
+        end
       else
         r.last << "\n"
         r << ''
       end
       r
     }.join
+  end
+
+  def self.word_size word
+    return 0 if word.empty?
+    word.chars.map{ |char|
+      if double_width?(char)
+        2
+      else
+        1
+      end
+    }.inject(&:+)
+  end
+
+  def self.double_width? char
+    return false if char.nil? || char && char.empty?
+    code = char.codepoints.to_a.first
+    code > 0xA0 && !(code > 0x452 && code < 0x1100)
   end
 
   def self.terminal_width
